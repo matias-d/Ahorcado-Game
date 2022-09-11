9a69a62ea53691f441f1fd086797007404cde677
Matias <matiasc.02.2003@gmail.com>
2022-09-11 20:08:42 -0300

    'Fixed'

--- scripts/index.js
+++ scripts/index.js
@@ -46,7 +46,7 @@ const incorrectLetter = letter =>{
 
 const missLetter = letter =>{
     mistakes++;
-    source = `./img/img${mistakes}.png`;
+    source = './img/img' + mistakes + '.png'
     img.src = source;
     
     endGame();
